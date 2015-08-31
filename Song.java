c6d9de9af2d5de615fcc58c3fc4ed1f2e689aa32
Sagira <Sagira@Sagira-PC>
2015-08-31 12:52:05 -0700

    My first commit

---
+++ Song.java
@@ -0,0 +1,17 @@
+package GitHub;
+
+public class Song {
+	void setArtist(String name) {
+	}
+
+	void setTitle(String song) {
+	}
+
+	public static void main(String[] args) {
+
+		Song t2 = new Song();
+		t2.setArtist("Maroon 5");
+		t2.setTitle("Sugar");
+		System.out.println("This song, full of life");
+	}
+}
